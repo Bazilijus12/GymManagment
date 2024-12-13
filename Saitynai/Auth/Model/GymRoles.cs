21af00b9924db5130952aa6347ba5d70fcb48cf7
Bazilijus12 <114340327+Bazilijus12@users.noreply.github.com>
2024-12-13 07:42:22 +0200

    a

--- Saitynai/Auth/Model/GymRoles.cs
+++ Saitynai/Auth/Model/GymRoles.cs
@@ -4,8 +4,7 @@
     {
         public const string Admin = nameof(Admin);
         public const string GymUser = nameof(GymUser);
-        public const string Coach = nameof(Coach);
 
-        public static readonly IReadOnlyCollection<string> All = new[] { Admin, GymUser, Coach };
+        public static readonly IReadOnlyCollection<string> All = new[] { Admin, GymUser };
     }
 }
